8c76ca5473c132e206269bea76bcbb1467891b6e
3370291812 <3370291812@qq.com>
2020-08-29 08:06:16 +0800

    添加项目文件。

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息通过下列特性集
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+' 检查程序集特性的值
+<Assembly: AssemblyTitle("GXXT_html")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("GXXT_html")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+<Assembly: Guid("bb806215-05f9-4b7b-bc36-a795b9ab9b25")>
+
+' 程序集的版本信息由下列四个值组成:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' 可以指定所有值，也可以使用“内部版本号”和“修订号”的默认值，
+' 方法是按如下所示使用 "*":
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
